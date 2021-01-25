f48ec52ea20be829773d9af79bc9d25b3f6a3525
Spaccesi <agustinspaccesi@gmail.com>
2021-01-25 19:26:42 +0100

    6. Categories

--- auctions/urls.py
+++ auctions/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
@@ -16,4 +13,4 @@ urlpatterns = [
     path("watchlist/<str:item_id>", views.watchlist, name="watchlist"),
     path("filter/<str:filter>", views.filter, name="filter"),
     path("close_auction/<str:item_id>", views.close_auction, name="close_auction"),
-] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- commerce/settings.py
+++ commerce/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import django_heroku
-import dj_database_url
-from decouple import config
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -52,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'commerce.urls'
@@ -132,9 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
